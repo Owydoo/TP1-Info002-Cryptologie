5604e1b0f81973474261fd4a04b5519a0ccc7e34
Hugo <owydoo@gmail.com>
2021-10-13 17:48:38 +0200

    Q3 for equal sizes

---
+++ README.md
@@ -0,0 +1,10 @@
+# TP Info002 Tom Kubasik, Hugo Hersemeule
+
+Nous sommes rendu a i2c avec size min = size max 
+
+## Question 2 : 
+go run . CALCUL_N ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 4 7
+
+## Indices et textes clairs
+go run . INDEX ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234
+-> RFV
--- hello.go
+++ hello.go
@@ -4,19 +4,39 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
 
 func main() {
-	arg1 := os.Args[1]
-	arg2 := os.Args[2]
 
-	switch arg1 {
-	case "MD5":
-		fmt.Printf("%x", hash_MD5(arg2))
-	case "SHA1":
-		fmt.Printf("%x", hash_SHA1(arg2))
+	H := os.Args[1]
+
+	switch H {
+	case "FP_MD5":
+		fmt.Printf("%x", hash_MD5(os.Args[2]))
+	case "FP_SHA1":
+		fmt.Printf("%x", hash_SHA1(os.Args[2]))
+	case "CALCUL_N": // Question 2
+		alphabet := os.Args[2]
+		size_min, _ := strconv.Atoi(os.Args[3])
+		size_max, _ := strconv.Atoi(os.Args[4])
+		fmt.Printf("%d", nb_posibilities(alphabet, size_min, size_max))
+	case "INDEX":
+		alphabet := os.Args[2]
+		index, _ := strconv.Atoi(os.Args[3])
+		fmt.Printf(getTextFromIndex(alphabet, index))
 	}
+
+}
+
+func nb_posibilities(alphabet string, size_min int, size_max int) int {
+	var n float64 = 0
+	for i := size_min; i <= size_max; i++ {
+		n += math.Pow(float64(len(alphabet)), float64(i))
+	}
+	return int(n)
 }
 
 func hash_MD5(text string) [16]byte {
@@ -27,4 +47,30 @@ func hash_MD5(text string) [16]byte {
 func hash_SHA1(text string) [20]byte {
 	data := []byte(text)
 	return sha1.Sum(data)
+}
+
+func getTextFromIndex(alphabet string, index int) string {
+	var len_alphabet = len(alphabet)
+	var alphabet_product = len_alphabet
+	var nbLetters int = 0
+	var indextemp int = index
+	var result string = ""
+	for indextemp >= alphabet_product {
+		indextemp -= alphabet_product
+		alphabet_product *= alphabet_product
+		nbLetters++
+	}
+	for i := 0; i <= nbLetters; i++ {
+		result = result + string(alphabet[indextemp%len(alphabet)])
+		indextemp /= len(alphabet)
+	}
+	return Reverse(result)
+
+}
+
+func Reverse(s string) (result string) {
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return
 }
